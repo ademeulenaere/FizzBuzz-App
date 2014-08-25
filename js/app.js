b0c647313cd4f2269ce14179946d3f17736cc5d1
ademeulenaere <Alex@alexdemeulenaere.cable.rcn.com>
2014-08-25 18:40:29 -0500

    1st edit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	function fizzbuzz() {
+		for(var i = 1; i <= 100; i++) {
+			console.log(i);
+			if(i % 5 === 0 && i % 3 === 0) {
+				/*(Print "fizzbuzz");*/
+				$('#empty').append('fizzbuzz');
+			}
+			else if (i % 3 === 0) {
+				/*(print "fizz")*/
+				$('#empty').append('fizz');
+			}
+			else if (i % 5 === 0) {
+				/*print "buzz")*/
+				$('#empty').append('buzz');
+			}
+		}
+	}
+});
+	
